fe6e21d596e73690435c74e69f44e00ed5e71eda
Mark Myers <markalope@gmail.com>
2018-05-20 15:10:04 -0700

    still cant get anything to display

--- js/app.js
+++ js/app.js
@@ -8,5 +8,15 @@ class App {
         this.productOption;
         this.totalViews = 0;
     }
+    render() {
+        const dom = appTemplate.content;
 
+        const productDisplay = dom.getElementById('product-display');
+    };
+
+    productDisplay.appendChild(productComponent.render());
+
+return dom;
 }
+
+
